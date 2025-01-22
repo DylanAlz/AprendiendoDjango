4fa2207a64e235bcb6b94d33a9ad344fe774dcb2
DYLAN STYV ALZATE BETANCUR <dylanalzate300037@correo.itm.edu.co>
2025-01-21 23:25:58 -0500

    render html files, jinja loops added, new view functions too.

--- tasker/urls.py
+++ tasker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.hola)
+    path('', views.hola),
+    path('projects/', views.Projects),
+    path('tasks/', views.Tasks)
 ]
--- tasker/views.py
+++ tasker/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Project, Task
 
 # Create your views here.
 def hola(request):
-    return HttpResponse("Hello proyect")
+    return render(request, 'index.html')
+
+def Projects(request):
+    projects = projects.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
+
+def Tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
